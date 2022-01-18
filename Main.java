994c38fc60dae41a261cc61ae97dcfb866e542c7
danimber <92602338+danimber@users.noreply.github.com>
2022-01-18 16:25:38 +0300

    Создал SecondClass.java
    Выводим Sheesh

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+public class Main implements FirstInter{
+    public static void main(String[] args) {
+        System.out.println(6);
+
+        int Num1 = 4;
         int Num2 = 6;
 
         System.out.println(Num1 + Num2);
